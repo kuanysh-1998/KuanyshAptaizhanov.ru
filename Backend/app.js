91ced9c8bfb843a93c295cdc4b59229e8d56cb57
Kuanysh <kuanysh.aptayzhanpov@mail.ru>
2023-02-09 10:16:05 +0300

    online

--- Backend/app.js
+++ Backend/app.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: "http://kuanyshaptaizhanov.ru",
+    origin: "http://kuanyshaptaizhanov.online",
   })
 );
 // Middlewares
