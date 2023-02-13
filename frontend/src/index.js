8c378b25338b62ed5b0b7b011b2d1025ca9e9e2e
Kuanysh Aptaizhanov <89320116+kuanysh-1998@users.noreply.github.com>
2023-02-13 21:58:56 +0300

    seo

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,13 +5,15 @@ import "react-toastify/dist/ReactToastify.css";
 import App from "./App";
 import store from "./redux/store";
 import { Provider } from "react-redux";
-import { HashRouter } from "react-router-dom";
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <Provider store={store} >
-            <App />
-        </Provider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <HelmetProvider>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </HelmetProvider>
+  </React.StrictMode>
 );
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -6,6 +6,7 @@ import Typewriter from "typewriter-effect";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchPosts } from "../../redux/apiCalls/postApiCall";
+import { Helmet } from "react-helmet-async";
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -16,40 +17,48 @@ const Home = () => {
     window.scrollTo(0, 0);
   }, []);
   return (
-    <section>
-      <div className="home">
-        <div className="home__header">
-          <div className="home__header-layout">
-            <h2 className="home__title">
-              Добро пожаловать на официальный сайт
-            </h2>
-            <h1 className="home__titlename">
-              <Typewriter
-                options={{
-                  autoStart: true,
-                  loop: true,
-                  delay: 160,
-                  strings: ["Куаныша Аптайжанова"],
-                }}
-              />
-            </h1>
+    <>
+      <Helmet>
+        <title>Kuanysh Aptaizhanov</title>
+        <meta name="description" content="Блог Куаныша Аптайжанова" />
+        <link rel="canonical" href="/" />
+      </Helmet>
+
+      <section>
+        <div className="home">
+          <div className="home__header">
+            <div className="home__header-layout">
+              <h2 className="home__title">
+                Добро пожаловать на официальный сайт
+              </h2>
+              <h1 className="home__titlename">
+                <Typewriter
+                  options={{
+                    autoStart: true,
+                    loop: true,
+                    delay: 160,
+                    strings: ["Куаныша Аптайжанова"],
+                  }}
+                />
+              </h1>
+            </div>
           </div>
+
+          <div className="home__latestpost">Последние публикации</div>
         </div>
 
-        <div className="home__latestpost">Последние публикации</div>
-      </div>
+        <div className="home__container">
+          <PostList posts={posts} />
+          <Sidebar />
+        </div>
 
-      <div className="home__container">
-        <PostList posts={posts} />
-        <Sidebar />
-      </div>
-
-      <div className="home__seepostslink">
-        <Link to="/posts" className="home__link">
-          Посмотреть все публикации
-        </Link>
-      </div>
-    </section>
+        <div className="home__seepostslink">
+          <Link to="/posts" className="home__link">
+            Посмотреть все публикации
+          </Link>
+        </div>
+      </section>
+    </>
   );
 };
 
